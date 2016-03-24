ed685ceca6b3f839c4bc77da7302164fcd7ae3c0
David Bochenski <david@goincremental.com>
2016-03-24 14:36:38 +0000

    added vpc module

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,77 +1,133 @@
+variable "account" {}
 variable "env" {}
-variable "tenant" {}
-variable "cidr_block" {}
-variable "zones" {
-  default = {
-    zone0 = "eu-west-1a"
-    zone1 = "eu-west-1b"
-    zone2 = "eu-west-1c"
-  }
+variable "cidr_octet_1" {
+    default = 10
+}
+variable "cidr_octet_2" {
+    default = 0
 }
 
+variable "zones" {
+    default = "eu-west-1a,eu-west-1b,eu-west-1c"
+}
+
+variable "dns_support" {
+    default = false
+}
+variable "dns_hostnames" {
+    default = false
+}
+
+################################################################################
+# VPC
+################################################################################
+
 resource "aws_vpc" "main" {
-  cidr_block = "${var.cidr_block}.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  cidr_block = "${var.cidr_octet_1}.${var.cidr_octet_2}.0.0/16"
+  enable_dns_support = "${var.dns_support}"
+  enable_dns_hostnames = "${var.dns_hostnames}"
   tags {
-    Name   = "${var.tenant}-${var.env}"
-    env    = "${var.env}"
-    tenant = "${var.tenant}"
+    Name     = "${var.account}-${var.env}-vpc"
+    CostCode = "${var.account}-${var.env}"
+    Env      = "${var.env}"
+    Account  = "${var.account}"
   }
 }
 
 # Each VPC can have only one internet gateway
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "main" {
     vpc_id = "${aws_vpc.main.id}"
-
     tags {
-        Name = "${var.tenant}-${var.env}"
+        Name = "${var.account}-${var.env}"
+        CostCode = "${var.account}-${var.env}"
     }
 }
+
+################################################################################
+# Public Route Table
+################################################################################
 
 # We define a route table that allows access to any ip address through
 # the internet gateway
-resource "aws_route_table" "internet_route_table" {
+resource "aws_route_table" "public" {
     vpc_id = "${aws_vpc.main.id}"
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.gw.id}"
+        gateway_id = "${aws_internet_gateway.main.id}"
     }
-
     tags {
-        Name = "InternetRouteTable"
+        Name     = "${var.account}-${var.env}-public"
+        CostCode = "${var.account}-${var.env}"
     }
 }
 
-# Each VPC should have a DMZ, connected to the internet gateway
-# The only machines we should put into that DMZ subnet should be our bastion
-# VPN server and / or a NAT server configured to allow machines in other
-# subnets to get access out to the internet.
-resource "aws_subnet" "dmz" {
+
+################################################################################
+# Public Subnets
+################################################################################
+
+# Each VPC should have a Public subnet per AZ, that can route to the internet 
+# gateway. The only machines we should put into that DMZ subnet should be our
+# bastion VPN server and / or a NAT server configured to allow machines in other
+# subnets to get access out to the internet.  ELBs as well perhaps?
+resource "aws_subnet" "public" {
     vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "${cidrsubnet("${var.cidr_block}.1.0/24", 2, count.index)}"
-    availability_zone = "${lookup(var.zones, concat("zone", count.index))}"
+    cidr_block = "${cidrsubnet("${var.cidr_octet_1}.${var.cidr_octet_2}.1.0/16", 8, count.index)}"
+    availability_zone = "${element(split(",",var.zones),count.index)}"
     tags {
-        Name = "${var.tenant}-${var.env}-dmz-${count.index}"
+        Name     = "${var.account}-${var.env}-public${format("%02d",count.index + 1)}"
+        CostCode = "${var.account}-${var.env}"
     }
-    count = 3
+    count = "${length(split(",",var.zones))}"
 }
 
-# We associate the route table with our dmz subnet, meaning only that
-# subnet will be able to communicate with the internet
-resource "aws_route_table_association" "dmz" {
-    subnet_id = "${element(aws_subnet.dmz.*.id, count.index)}"
-    route_table_id = "${aws_route_table.internet_route_table.id}"
-    count = 3
+# We associate the public route table with our public subnets, meaning only these
+# subnets will be able to communicate directly with the internet
+resource "aws_route_table_association" "public" {
+    subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+    route_table_id = "${aws_route_table.public.id}"
+    count = "${length(split(",",var.zones))}"
 }
 
-# We create subnets to hold our application layer, and
-resource "aws_subnet" "app" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "${cidrsubnet("${var.cidr_block}.2.0/24", 2, count.index)}"
-    availability_zone = "${lookup(var.zones, concat("zone", count.index))}"
-    tags {
-        Name = "${var.tenant}-${var.env}-app-${count.index}"
-    }
-    count = 3
+
+################################################################################
+# NAT Gateways
+################################################################################
+
+# We launch a nat gateway in each of the public subnets so that (if desired) 
+# private subnets can route out to the internet via these gateways
+
+resource "aws_eip" "nat" {
+  count = "${length(split(",",var.zones))}"
+  vpc = true
 }
+
+resource "aws_nat_gateway" "nat" {
+  count = "${length(split(",",var.zones))}"
+  allocation_id = "${element(aws_eip.nat.*.id,count.index)}"
+  subnet_id = "${element(aws_subnet.public.*.id,count.index)}"
+  depends_on = ["aws_internet_gateway.main"]
+}
+
+
+################################################################################
+# Private Route Tables
+################################################################################
+
+# we need one private route table per AZ since each will route internet traffic
+# through a separate NAT gateway also associated with that AZ to ensure we 
+# maintain connectivity if a single AZ goes down
+
+resource "aws_route_table" "private" {
+  count = "${length(split(",",var.zones))}"
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block  = "0.0.0.0/0"
+    nat_gateway_id = "${element(aws_nat_gateway.nat.*.id, count.index)}"
+  }
+  tags {
+    Name     = "${var.account}-${var.env}-private"
+    CostCode = "${var.account}-${var.env}"
+  }
+}
+output "private_routetables" {value = "${join(",",aws_route_table.private.*.id)}"}
