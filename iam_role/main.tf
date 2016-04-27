f678e7a82d91393b567bd2444398bc31d782ad3a
David Bochenski <david@goincremental.com>
2016-04-27 16:23:30 +0100

    Basic iam role to be referenced in other modules

---
+++ iam_role/main.tf
@@ -0,0 +1,21 @@
+variable "account" {}
+variable "env" {}
+variable "svc_code" {}
+variable "flag" {
+  default = 0
+}
+
+resource "aws_iam_role" "svc_role" {
+  name = "${var.account}-${var.env}-${var.svc_code}"
+  count = "${var.flag}"
+  assume_role_policy =  "${file("${path.module}/AssumeRole.json")}"
+}
+
+resource "aws_iam_instance_profile" "svc_profile" {
+  name = "${var.account}-${var.env}-${var.svc_code}"
+  count = "${var.flag}"  
+  roles = ["${aws_iam_role.svc_role.name}"]
+}
+
+output "name" {value = "${aws_iam_role.svc_role.name}"}
+output "profile_id" {value = "${aws_iam_instance_profile.svc_profile.id}"}
